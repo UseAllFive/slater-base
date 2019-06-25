faa2dd9a84fb9bf5117160a0c903ee65070f6e35
bretmorris <bret@useallfive.com>
2019-06-25 14:54:29 -0700

    Prep for scss

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,28 +1,7 @@
 import '../styles/main.scss'
 import '@/lib/select.js'
 import app from '@/app.js'
-//import router from '@/router.js'
 import { fetchCart } from '@/lib/cart.js'
-
-/**
- * store binding fn
- */
-// const images = lazim()
-
-/**
- * bind on page load
- */
-// images()
-
-// router.on('after', () => {
-//   app.unmount()
-//   app.mount()
-
-//   /**
-//    * bind new images
-//    */
-//   images()
-// })
 
 /**
  * load any data that your site needs on fist load
